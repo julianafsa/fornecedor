b062f89782b1c2761bd2436a79a72ea650cbde15
Juliana Aquino <juliana.aquino@santander.com.br>
2021-11-04 16:43:03 -0300

    aula 04

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO info_fornecedor (endereco, estado, nome) VALUES 
+('Endereco do fornecedor de GO','GO','Fornecedor-GO'); 
+
+INSERT INTO produto (descricao, estado, nome, preco) VALUES 
+('Rosa','DF','Rosas', '1.99'),
+('Orquídea','DF','Orquídeas', '25.00'),
+('Girassol','DF','Girassol', '5.00'); 
